a1e4bcba2141bb9958690503036ba157a18abbc4
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-03-28 22:32:25 +0530

    Update App.jsx

--- assistant-UI/src/App.jsx
+++ assistant-UI/src/App.jsx
@@ -17,7 +17,7 @@ export default function DatabaseUI() {
   const [selectDatabase, setSelectDatabase] = useState(database[0]);
   const [validDatabase, setValidDatabase] = useState(false);
   const datbaseBaseUrl = {
-    PostgreSQL: "http://65.0.5.182:5000/postgresql",
+    PostgreSQL: "https://database-manager-with-postgresql.onrender.com/postgresql",
     MySQL: "",
     Oracle: "",
   };
